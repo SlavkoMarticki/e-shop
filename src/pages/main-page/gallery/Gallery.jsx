e5910c190b06f8b0cc8b1b8e79e5fbb795a436de
Slavko <martickis@gmail.com>
2023-02-26 20:59:50 +0100

    commit

--- src/pages/main-page/gallery/Gallery.jsx
+++ src/pages/main-page/gallery/Gallery.jsx
@@ -1,10 +1,17 @@
 import "./gallery.css";
-
+import tool1 from "../../../assets/images/tool1.jpg";
+import tool2 from "../../../assets/images/tool2.jpg";
+import tool3 from "../../../assets/images/tool3.jpg";
 export default function Gallery(){
     return(
         <div className="gallery-container">
-             <div className="gallery-header">
+            <div className="gallery-header">
                 <p>Gallery</p>
+            </div>
+            <div className="images-holder">
+                <img src={tool1} alt="" />
+                <img src={tool2} alt="" />
+                <img src={tool3} alt="" />
             </div>
         </div>
     );
